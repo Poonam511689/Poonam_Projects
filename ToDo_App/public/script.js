73b4339b767af4fcc8e39cb989a14948b1a1616b
Poonam511689 <poonam.23cse@bmu.edu.in>
2025-06-29 16:34:40 +0530

    script.js

--- ToDo_App/public/script.js
+++ ToDo_App/public/script.js
@@ -3,10 +3,48 @@ const newtask = document.querySelector('#newtask');
 const tasklist = document.querySelector('.tasklist');
 
 function addToDom(todos) {
-    tasklist.innerHTML = ""; // Clear every time
+    tasklist.innerHTML = ""; // Clear list
+
     todos.forEach(element => {
         const li = document.createElement('li');
         li.innerText = element.name;
+
+        // Apply "completed" class if task is marked done
+        if (element.completed) {
+            li.style.textDecoration = "line-through";
+            li.style.opacity = 0.6;
+        }
+
+        // ✅ Complete button
+        const doneBtn = document.createElement('button');
+        doneBtn.innerHTML = '✔️';
+        doneBtn.style.marginRight = '8px';
+        doneBtn.onclick = () => {
+            axios.post('/completetodo', { id: element.id })
+                .then((res) => {
+                    addToDom(res.data);
+                })
+                .catch((err) => console.error(err));
+        };
+
+        // ❌ Delete button
+        const delBtn = document.createElement('button');
+        delBtn.innerHTML = '&times;';
+        delBtn.onclick = () => {
+            axios.post('/deletetodo', { id: element.id })
+                .then((res) => {
+                    addToDom(res.data);
+                })
+                .catch((err) => console.error(err));
+        };
+
+        const btnGroup = document.createElement('div');
+        btnGroup.appendChild(doneBtn);
+        btnGroup.appendChild(delBtn);
+
+        li.innerHTML = element.name;
+        li.appendChild(btnGroup);
+
         tasklist.appendChild(li);
     });
 }
@@ -44,3 +82,4 @@ axios.get('/gettodos')
 .catch((err) => {
     console.error(err);
 });
+
